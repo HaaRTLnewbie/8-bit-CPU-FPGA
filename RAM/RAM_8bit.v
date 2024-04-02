4c7a036db1c7d4047425971b4e9c65a4fc5a2114
Haaris <160754554+HaaRTLnewbie@users.noreply.github.com>
2024-04-02 14:25:22 +0100

    Update RAM_8bit.v

--- RAM/RAM_8bit.v
+++ RAM/RAM_8bit.v
@@ -1,7 +1,7 @@
 module RAM_8bit
 #(parameter WIDTH = 8)
 (
-	input		wr_en, rd_en, clk,
+	input		clk, wr_en, rd_en,		// wr_en and rd_en are enable high
 	input		[(WIDTH / 2):0] addr,		// 4-bit address from MAR
 	inout		[WIDTH - 1:0] bus		// 8-bit bidirectional bus
 );
