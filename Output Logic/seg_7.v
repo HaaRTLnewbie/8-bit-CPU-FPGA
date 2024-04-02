4a3fb810cc31c042d84039135445fbb5c4825f0a
Haaris <160754554+HaaRTLnewbie@users.noreply.github.com>
2024-04-02 14:34:56 +0100

    Update seg_7.v

--- Output Logic/seg_7.v
+++ Output Logic/seg_7.v
@@ -2,7 +2,7 @@ module seg_7
 (
 	input 		clk,
 	input		[11:0] bcd,
-	output reg 	[6:0] d0, d1, d2  	// Output registers to drive 3, 7-seg displays
+	output reg 	[6:0] d0, d1, d2  	// Output registers to drive 3, 7-segment displays
 );
 
 localparam	ZERO	= 7'b1111110,
