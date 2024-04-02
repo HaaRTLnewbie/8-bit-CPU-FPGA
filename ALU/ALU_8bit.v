5bd39aee3528ef816e68320a6e8f59a75531f195
Haaris <160754554+HaaRTLnewbie@users.noreply.github.com>
2024-04-02 14:16:09 +0100

    Update ALU_8bit.v

--- ALU/ALU_8bit.v
+++ ALU/ALU_8bit.v
@@ -4,7 +4,7 @@ module ALU_8bit
 input		clk,									  
 input		[1:0] sel,				// Select signal to choose operation to execute
 input		[WIDTH - 1:0] reg_A, reg_B,		// 8-bit inputs directly from register A and B
-	inout		[WIDTH - 1:0] bus		// 8-bit bidirectional bus
+inout		[WIDTH - 1:0] bus			// 8-bit bidirectional bus
 );
 
 localparam	ADD	=	2'b00,			// Parameterising opcodes
@@ -12,9 +12,9 @@ localparam	ADD	=	2'b00,			// Parameterising opcodes
 		MLT	=	2'b10,
 		DIV	=	2'b11;
 
-reg [WIDTH - 1:0] ALU_reg;				// Defining a single 8-bit wide memory
+reg [WIDTH - 1:0] ALU_reg;				// Defining an 8-bit wide register
 
-assign bus = en ? ALU_reg : 8'bz;			// Only outputs register to the bus when enable is high
+assign bus = en ? ALU_reg : 8'bz;			// Tri-state buffer to set high-impedance to bus if en is low
 
 always @ (posedge clk)
 case(sel)						// Instruction select
