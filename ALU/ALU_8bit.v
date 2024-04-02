efb7b0c675cb39921fca5993bfcb717a5fb91304
Haaris <160754554+HaaRTLnewbie@users.noreply.github.com>
2024-04-02 12:58:20 +0100

    Update ALU_8bit.v

--- ALU/ALU_8bit.v
+++ ALU/ALU_8bit.v
@@ -1,28 +1,26 @@
 module ALU_8bit
 #(parameter WIDTH = 8)
 (
-input		clk, clr, en,										  //Clock, clear, and enable are 
-input		[1:0] sel,											  //Select signal to choose operation to execute
-input		[WIDTH - 1:0] reg_A, reg_B,			  //8-bit inputs directly from register A and B
-inout		[WIDTH - 1:0] bus_io
+input		clk,									  
+input		[1:0] sel,				// Select signal to choose operation to execute
+input		[WIDTH - 1:0] reg_A, reg_B,		// 8-bit inputs directly from register A and B
+	inout		[WIDTH - 1:0] bus		// 8-bit bidirectional bus
 );
 
-localparam	ADD	=	2'b00,								  //Parameterising names for the opcodes
-			    	SUB	=	2'b01,
-			    	MLT	=	2'b10,
-			    	DIV	=	2'b11;
+localparam	ADD	=	2'b00,			// Parameterising opcodes
+		SUB	=	2'b01,
+		MLT	=	2'b10,
+		DIV	=	2'b11;
 
-reg [WIDTH - 1:0] ALU_reg;							  //Defining a single 8-bit wide memory
+reg [WIDTH - 1:0] ALU_reg;				// Defining a single 8-bit wide memory
 
-assign bus = en ? ALU_reg : 8'bz;				  //Only outputs register to the bus when enable is high
+assign bus = en ? ALU_reg : 8'bz;			// Only outputs register to the bus when enable is high
 
 always @ (posedge clk)
-	begin
-    case(sel)												      //Case to select instruction
-			ADD		:	ALU_reg = reg_A + reg_B;
-			SUB		:	ALU_reg = reg_A - reg_B;
-			MLT		: 	ALU_reg = reg_A * reg_B;
-			DIV		:	ALU_reg = reg_A / reg_B;
-		endcase													      //Default not required as all states are considered
-	end
+case(sel)						// Instruction select
+	ADD	:	ALU_reg <= reg_A + reg_B;
+	SUB	:	ALU_reg <= reg_A - reg_B;
+	MLT	: 	ALU_reg <= reg_A * reg_B;
+	DIV	:	ALU_reg <= reg_A / reg_B;
+endcase							// Default not required as all states are considered (with current instruction set)
 endmodule
