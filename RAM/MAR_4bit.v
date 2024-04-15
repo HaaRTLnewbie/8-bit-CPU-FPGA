5952eb0deaa9f4e576abf130c0c3d90675e62f9a
Haaris <160754554+HaaRTLnewbie@users.noreply.github.com>
2024-04-15 10:58:23 +0100

    Update MAR_4bit.v

--- RAM/MAR_4bit.v
+++ RAM/MAR_4bit.v
@@ -1,20 +1,25 @@
 module MAR_4bit
 #(parameter WIDTH = 4)
 (
-	input 	clk, clr, prog_en, reg_en,		// clr, prog_en, and reg_en are enable high
-	input	[WIDTH - 1:0] sw, bus,			// 4 input dip switch and 4-bit bus input
-	output	prog,					
-	output	[WIDTH - 1:0] addr			// 4-bit addr output to RAM module
+	input 		clk,
+			clr,				// enable high
+			prog_en,			// enable high
+			reg_en,				// enable high
+				
+	input		[WIDTH - 1:0] sw,		// 4 input dip switch
+	
+	input		[WIDTH - 1:0] bus,		// 4-bit bus input
+		
+	output		[WIDTH - 1:0] addr		// 4-bit addr output to RAM module
 );
 
 reg [WIDTH - 1:0] mem_addr;
 
 assign addr = (prog_en) ? sw : mem_addr;
-assign prog = prog_en;					// Drives prog_en to the prog net to go to RAM module
 	
 always @ (posedge clk or posedge clr)			// Procedural block triggering on rising edge of clk or clr for asynchronous reset
 begin
-	if (clr) 		mem_addr <= 8'b0;
+	if (clr) 		mem_addr <= 4'b0;
 	else if (reg_en)	mem_addr <= bus;
 end
 
